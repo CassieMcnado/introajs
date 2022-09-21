679f9002c134aa7f4c1e3fc0e992b3e53f2a427d
Cassie Mcnado <madnessshaw@gmail.com>
2022-09-20 19:07:19 -0500

    js de bank account

---
+++ bank-account.js
@@ -0,0 +1,20 @@
+//obtenion datos login , calculo de cuentas , mostrar en pantalla js, actualizacion de cookes, fin de sesion, validar errores
+
+function loadUserDara() {
+    let user = getSession();
+    //pinta datos en panatalla
+    document.getElementById
+}
+
+
+function deposit () {
+    if(checkExpiration()){
+    //condiciones y reglas del monto del usuario
+    }
+}
+
+function withdraw(){
+    if (checkExpiration()){
+        //suma del monto del usuario
+    }
+}
